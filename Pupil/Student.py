21f94d305a1f49e89f7110ccae4662a09e1bf857
stefan1995mm <dominik.stefanczyk@gmail.com>
2018-05-22 17:57:40 +0200

    Start writing Student class

--- Pupil/Student.py
+++ Pupil/Student.py
@@ -1,12 +1,12 @@
 class Pupil:
     def __init__(self, imie, nazwisko):
-        self.__name = imie
-        self.__surname = nazwisko
-        self.__marks = {}
-
+        self.name = imie
+        self.surname = nazwisko
+        self.marks = {}
+        self.oceny = [1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6]
     @property
     def name(self):
-        return self.__name
+        return self._name
 
     @name.setter
     def name(self, nowe_imie):
@@ -17,11 +17,11 @@ class Pupil:
         elif len(nowe_imie) < 3:
             print("Imie musi mieć conajmniej 3 znaki")
         else:
-            self.__name = nowe_imie
+            self._name = nowe_imie
 
     @property
     def surname(self):
-        return self.__surname
+        return self._surname
 
     @surname.setter
     def surname(self, nowe_nazwisko):
@@ -32,31 +32,35 @@ class Pupil:
         elif len(nowe_nazwisko) < 3:
             print("Nazwisko musi mieć conajmniej 3 znaki")
         else:
-            self.__surname = nowe_nazwisko
+            self._surname = nowe_nazwisko
 
     def print_marks(self):
-        for x in self.__marks.keys():
-            print(x + "\t\t" + str(self.__marks[x]))
+        for x in self.marks.keys():
+            print(x + "\t\t" + str(self.marks[x]))
 
     def mean(self):
         mean = 0
-        for x in self.__marks.values():
+        for x in self.marks.values():
             mean += x
-        return (mean / len(self.__marks))
+        return (mean / len(self.marks))
 
     def complete_marks(self, nowa_ocena, liczba):
-        if isinstance(nowa_ocena, str) and isinstance(liczba, (int, float)):
-            self.__marks[nowa_ocena] = liczba
+        if isinstance(nowa_ocena, str) and isinstance(liczba, (int, float)) and liczba in self.oceny:
+            self.marks[nowa_ocena] = liczba
         else:
             print("Podales zle dane")
 
     def __str__(self):
-        opis = "Imie: " + self.__name + "\n"
-        opis += "Nazwisko: " + self.__surname + "\n"
+        opis = "Imie: " + self.name + "\n"
+        opis += "Nazwisko: " + self.surname + "\n"
         opis += "Srednia ocen: " + str(self.mean())
         return opis
 
 
+class Student:
+
+
+# klasa = Pupil("J",3)
 klasa = Pupil("Jan", "Kowalski")
 # klasa.name = ""
 # print(klasa.name)
@@ -74,9 +78,14 @@ klasa = Pupil("Jan", "Kowalski")
 # print(klasa.surname)
 # klasa.surname = "Nowak"
 # print(klasa.surname)
-klasa.print_marks()
+# klasa.print_marks()
+# klasa.complete_marks("Polski", 4.2)
+# klasa.complete_marks(3, "ang")
+# klasa.complete_marks("test", "ang")
+# klasa.complete_marks(3, 5)
 klasa.complete_marks("Polski", 4)
-klasa.complete_marks(3, "ang")
-klasa.complete_marks("test", "ang")
-klasa.complete_marks(3, 5)
+klasa.complete_marks("Angielski", 3)
+klasa.complete_marks("Matematyka", 3.5)
+klasa.complete_marks("Fizyka", 6)
+# klasa.print_marks()
 print(klasa)
